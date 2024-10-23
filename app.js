cf7e434ac611f076f35700b85d0fba760485d664
Hadeyfisayoh <adebisienoch5@gmail.com>
2024-10-23 17:23:31 +0100

    Initial commit

---
+++ app.js
@@ -0,0 +1,17 @@
+const express = require("express");
+const app = express();
+const tasks = require("./routes/tasks");
+
+app.use(express.json());
+
+app.get("/hello", (req, res) => {
+  res.send("Tak manager app");
+});
+
+app.use("/api/v1/tasks", tasks);
+
+const port = 5000;
+
+app.listen(port, () => {
+  console.log(`Server is listening at ${port}....`);
+});
