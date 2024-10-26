301338cdbb6baa07b5477e659cfce53c2c5f3ab4
Hadeyfisayoh <adebisienoch5@gmail.com>
2024-10-26 17:05:55 +0100

    dd

--- app.js
+++ app.js
@@ -4,11 +4,8 @@ const tasks = require("./routes/tasks");
 const connectDB = require("./db/connect");
 require("dotenv").config();
 
+app.use(express.static("./public"));
 app.use(express.json());
-
-app.get("/hello", (req, res) => {
-  res.send("Tak manager app");
-});
 
 app.use("/api/v1/tasks", tasks);
 
